5a5b52c0a9e8a0f695e20eab3d36eca595b6b281
iliyanzan <iliyan.zankinski@gmail.com>
2017-02-13 11:30:21 +0200

    ExponentRegulatedSin, output, encog shutdown

--- src/digitsresilient/DigitsResilient.java
+++ src/digitsresilient/DigitsResilient.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.encog.Encog;
 import org.encog.engine.network.activation.ActivationBipolarSteepenedSigmoid;
 import org.encog.engine.network.activation.ActivationElliottSymmetric;
 import org.encog.engine.network.activation.ActivationFunction;
@@ -22,9 +23,9 @@ import org.encog.neural.networks.training.propagation.resilient.ResilientPropaga
 
 public class DigitsResilient {
 
-	private static final long MAX_TRAINING_TIME = 30000;
+	private static final long MAX_TRAINING_TIME = 1000;
 
-	private static final int MAX_EPOCHS = 20000;
+	private static final int MAX_EPOCHS = 300000;
 
 	private static final double TARGET_ANN_ERROR = 0.0001;
 
@@ -66,7 +67,7 @@ public class DigitsResilient {
 	}
 
 	private static void experiment(String title, ActivationFunction activation, NeuralDataSet training, double epsilon) {
-		for (long g = 0; g < 30; g++) {
+		for (long g = 0; g < 10; g++) {
 			BasicNetwork network = new BasicNetwork();
 			network.addLayer(new BasicLayer(activation, true, INPUT_SIZE));
 			network.addLayer(new BasicLayer(activation, true, HIDDEN_SIZE));
@@ -77,24 +78,35 @@ public class DigitsResilient {
 			final Train train = new ResilientPropagation(network, training);
 
 			int epoch = 1;
-			long start = System.currentTimeMillis();
-			
-			do {
-				train.iteration();
-				epoch++;
-			} while (train.getError() > epsilon && (System.currentTimeMillis() - start) < MAX_TRAINING_TIME && epoch < MAX_EPOCHS);
 
-			System.out.println(title + "\t" + (System.currentTimeMillis() - start) + "\t" + epoch + "\t" + train.getError());
+			System.out.println(title + "\t" + g);
+
+			for (int i = 0; i < 60; ++i) {
+				long start = System.currentTimeMillis();
+				do {
+					train.iteration();
+					epoch++;
+				} while (train.getError() > epsilon && (System.currentTimeMillis() - start) < MAX_TRAINING_TIME && epoch < MAX_EPOCHS);
+
+				System.out.println(train.getError());
+			}
+
+			System.out.println("epochs:\t" + epoch);
+			System.out.println();
+			train.finishTraining();
 		}
 	}
 
 	public static void main(final String args[]) {
 		experiment("Fading Sine", new ActivationFadingSin(1), MINUS_PLUS_ONE_TRAINING, TARGET_ANN_ERROR);
+		experiment("Exponent Regulated Sin", new ActivationExponentRegulatedSin(1), MINUS_PLUS_ONE_TRAINING, TARGET_ANN_ERROR);
 		experiment("Sigmoid", new ActivationSigmoid(), ZERO_ONE_TRAINING, TARGET_ANN_ERROR);
 		experiment("Bipolar Sigmoid", new ActivationBipolarSteepenedSigmoid(), MINUS_PLUS_ONE_TRAINING, TARGET_ANN_ERROR);
 		experiment("Logarithm", new ActivationLOG(), MINUS_PLUS_ONE_TRAINING, TARGET_ANN_ERROR);
 		experiment("Hyperbolic Tangent", new ActivationTANH(), MINUS_PLUS_ONE_TRAINING, TARGET_ANN_ERROR);
 		experiment("Elliott Symmetric", new ActivationElliottSymmetric(), MINUS_PLUS_ONE_TRAINING, TARGET_ANN_ERROR);
+
+		Encog.getInstance().shutdown();
 	}
 
 }
@@ -167,4 +179,74 @@ class ActivationFadingSin implements ActivationFunction {
 		return "fading sin";
 	}
 
+}
+
+class ActivationExponentRegulatedSin implements ActivationFunction {
+
+	private final ActivationSIN SIN = new ActivationSIN();
+
+	private double period = 1.0D;
+
+	static final double LOW = -0.99;
+
+	static final double HIGH = +0.99;
+
+	public ActivationExponentRegulatedSin(double period) {
+		this.period = period;
+	}
+
+	@Override
+	public void activationFunction(double[] values, int start, int size) {
+		for (int i = start; i < (start + size) && i < values.length; i++) {
+			double x = values[i] / period;
+
+			values[i] = Math.PI * BoundMath.sin(x) / BoundMath.exp(Math.abs(x));
+		}
+	}
+
+	@Override
+	public double derivativeFunction(double before, double after) {
+		double x = before / period;
+
+		if (x == 0) {
+			return Double.MAX_VALUE;
+		}
+
+		return Math.PI * BoundMath.exp(-Math.abs(x)) * (BoundMath.cos(x) * Math.abs(x) - x * BoundMath.sin(x)) / Math.abs(x);
+	}
+
+	@Override
+	public ActivationFunction clone() {
+		return new ActivationExponentRegulatedSin(period);
+	}
+
+	@Override
+	public String getFactoryCode() {
+		return null;
+	}
+
+	@Override
+	public String[] getParamNames() {
+		return SIN.getParamNames();
+	}
+
+	@Override
+	public double[] getParams() {
+		return SIN.getParams();
+	}
+
+	@Override
+	public boolean hasDerivative() {
+		return true;
+	}
+
+	@Override
+	public void setParam(int index, double value) {
+		SIN.setParam(index, value);
+	}
+
+	@Override
+	public String getLabel() {
+		return "exponent regulated sin";
+	}
 }
